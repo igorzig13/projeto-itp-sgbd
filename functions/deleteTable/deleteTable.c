90cc064db49bde21bc580dd5518f68970c2af471
Igor Marques <igorzig13@gmail.com>
2023-12-10 11:43:03 -0300

    deleteTable checa se existe tabela a ser deletada.

--- functions/deleteTable/deleteTable.c
+++ functions/deleteTable/deleteTable.c
@@ -8,15 +8,29 @@ void deleteTable(){
     printf("* Digite o nome da tabela a ser apagada:\n");
     scanf(" %s", nome);
 
+    char check = ' ';
+    while(isNameAvailable(nome) == 0){
+        printf("* Atenção: Não foi encontrada uma tabela com esse nome! Tentar novamente? (s/n)");
+        scanf(" %c", &check);
+        if (check == 's'){
+            printf("* Digite o nome da tabela a ser apagada:\n");
+            scanf(" %s", nome);
+        } else {
+            system("clear");
+            printf("* Retornando ao Menu Principal...\n");
+            return;
+        }
+    }
+
     char caminho[100] = "../tables/";
     strcat(caminho, nome);
     strcat(caminho, ".txt");
 
     printf("* Atenção: Essa ação deletará permanentemente a tabela %s e todos os seus dados, deseja "
            "prosseguir? (s/n)\n", nome);
-    char confirmacao = ' ';
-    scanf(" %c", &confirmacao);
-    if (confirmacao == 's'){
+    check = ' ';
+    scanf(" %c", &check);
+    if (check == 's'){
         remove(caminho);
         remove_table(nome);
     } else {
--- functions/deleteTable/deleteTable.h
+++ functions/deleteTable/deleteTable.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../updateTableList/updateTableList.h"
+#include "../../utils/isNameAvailable/isNameAvailable.h"
 
 void deleteTable();
 
